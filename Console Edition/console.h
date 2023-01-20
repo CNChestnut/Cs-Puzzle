36236b4567d3005e060665992d10aaa0833ef48b
Chestnut Li <cn_l_chestnut@163.com>
2023-01-20 15:25:36 +0800

    完成 About 界面
    1.完成 About 界面
    2.优化界面
    3.其他略改

--- Console Edition/console.h
+++ Console Edition/console.h
@@ -1,40 +1,67 @@
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <conio.h>
 #include "../main.h"
+
+void p_newGame();
+void p_about();
 
 void menu()
 {
 	char op;
 	do
 	{
+		system("cls");
 		printf("\n\n\n");
 		printf("\t%s\n\n", program_info_loaded.program_name);
-		printf("\t新游戏/教程[N]\n");
-		printf("\t加 载 游 戏[L]\n");
-		printf("\t编 辑 地 图[M]\n");
-		printf("\t关 于 游 戏[A]\n");
-		printf("\t退 出 游 戏[E]\n");
-		op = _getch();
+		printf("\t新游戏/教程  [N]\n");//ing
+		printf("\t加 载 游 戏  [L]\n");
+		printf("\t编 辑 地 图  [M]\n");
+		printf("\t关 于 游 戏  [A]\n");//ok
+		printf("\t退 出 游 戏  [E]\n");//ok
+		op = getch();
 		switch (op)
 		{
-		case 'N':
-		case 'n':
-			printf("developing");
-			break;
-		case 'L':
-		case 'l':
-			printf("developing");
-			break;
-		case 'M':
-		case 'm':
-			printf("developing");
-			break;
-		case 'A':
-		case 'a':
-			printf("developing");
-			break;
+			case 'N':
+			case 'n':
+				p_newGame();
+				break;
+			case 'L':
+			case 'l':
+				printf("developing");
+				break;
+			case 'M':
+			case 'm':
+				printf("developing");
+				break;
+			case 'A':
+			case 'a':
+				p_about();
+				break;
 		}
 	}
-	while (op != 'E' & op != 'e');
+	while ((op != 'E') & (op != 'e'));
+}
+
+void p_newGame()
+{
+	system("cls");
+	printf("\n\n\n");
+	printf("\t新游戏/教程\n\n");
+	getch();
+	
+}
+
+void p_about()
+{
+	system("cls");
+	printf("\n\n\n");
+	printf("\t关于 %s\n\n",program_info_loaded.program_name);
+	printf("\t 版  本 :%s\n",program_info_loaded.program_version);
+	printf("\t发行时间:%s\n",program_info_loaded.program_buildTime);
+	printf("\t 作  者 :%s\n",program_info_loaded.auth_name);
+	printf("\t作者网站:%s\n\n",program_info_loaded.auth_site);
+	printf("\t[Press any kry to exit]");
+	getch();
 }
--- Console Edition/main.cpp
+++ Console Edition/main.cpp
@@ -2,7 +2,6 @@
 #include <iostream>
 #include <string.h>
 #include "../SimpleIni.h"
-#include "../main.h"
 #include "console.h"
 
 using namespace std;
